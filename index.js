910707dca443911a6c80f6280cc0a45899aacdda
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2020-01-27 14:35:25 -0500

    created website skeleton

---
+++ index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <script type="text/javascript">
+      getheadinfo();
+    </script>
+  </head>
+  <body>
+    <h1>Welcome to the Team Chaos Website!</h1>
+  </body>
+</html>
