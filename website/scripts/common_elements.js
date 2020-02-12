d006a775a2e984034a778ea82e8c148b75d49a0e
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2020-02-12 14:30:48 -0500

    added website template

--- website/scripts/common_elements.js
+++
@@ -1,9 +0,0 @@
-<!-- head info -->
-var getheadinfo =
-{
-  document.write('
-    <meta charset="utf-8">
-    <link rel='stylesheet' type='text/css' href='stylesheet.css'>
-    <title>Chaos Automata</title>
-  ')
-}
