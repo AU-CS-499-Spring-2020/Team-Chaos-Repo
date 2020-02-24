2c5307fc43c3e0694b1c944bddc7752a8047d8c9
Brennen Nalley <46981701+Rebnnenx@users.noreply.github.com>
2020-02-24 10:36:24 -0500

    Update StringGenerator.js

--- program/StringGenerator.js
+++ program/StringGenerator.js
@@ -2,4 +2,30 @@
 class StringGenerator {
 
     
+    //constructor
+    constructor(seed){
+        this.seed = seed;
+        this.input= getInput(seed);
+    }
+
+    constructor(){
+        this.seed="";
+        this.input="";
+    }
+
+    setSeed(seed){
+        this.seed= seed;
+        this.setInput(seed);
+    }
+
+    getSeed(){
+        return this.seed;
+    }
+
+    setInput(seed){
+        this.input= seed
+        while (this.input.length()  < 1000000){
+            this.input=this.input + seed;
+        }
+    }
 }
